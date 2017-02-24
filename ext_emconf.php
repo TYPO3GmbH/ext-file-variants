fd5e96feb49c1124a53f73fec1262b6b26ed1278
Anja Leichsenring <aleichsenring@ab-softlab.de>
2017-02-24 12:17:29 +0100

    Initial import

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+/************************************************************************
+ * Extension Manager/Repository config file for ext "file_variants".
+ ************************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Translatable files',
+    'description' => 'Files can present their language variants and use them',
+    'category' => 'extension',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '8.6.0-8.99.99'
+        ),
+        'conflicts' => array(),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'T3G\\AgencyPack\\FileVariants\\' => 'Classes',
+        ),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Anja Leichsenring',
+    'author_email' => 'anja.leichsenring@typo3.com',
+    'author_company' => 'TYPO3 GmbH',
+    'version' => '1.0.0',
+);
