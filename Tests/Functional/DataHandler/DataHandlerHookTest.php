8efc8f426fbc23e47a9c65afbb410071905664fa
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-02-26 18:41:58 +0100

    [TASK] First functional test

--- Tests/Functional/DataHandler/DataHandlerHookTest.php
+++ Tests/Functional/DataHandler/DataHandlerHookTest.php
@@ -14,11 +14,46 @@ namespace T3G\AgencyPack\FileVariants\Tests\Functional\DataHandler;
  *
  * The TYPO3 project - inspiring people to share!
  */
-use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3\CMS\Core\Resource\Security\FileMetadataPermissionsAspect;
+use TYPO3\CMS\Core\Tests\Functional\DataHandling\AbstractDataHandlerActionTestCase;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
   * Description
   */
-class DataHandlerHookTest extends FunctionalTestCase {
+class DataHandlerHookTest extends AbstractDataHandlerActionTestCase {
+
+    protected $testExtensionsToLoad = ['typo3conf/ext/file_variants'];
+
+    /**
+     * @var string
+     */
+    protected $scenarioDataSetDirectory = 'typo3conf/ext/file_variants/Tests/Functional/DataHandler/DataSet/';
+
+    /**
+     * @var string
+     */
+    protected $assertionDataSetDirectory = 'typo3conf/ext/file_variants/Tests/Functional/DataHandler/Modify/DataSet/';
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $fileMetadataPermissionAspect = $this->prophesize(FileMetadataPermissionsAspect::class);
+        GeneralUtility::setSingletonInstance(FileMetadataPermissionsAspect::class, $fileMetadataPermissionAspect->reveal());
+
+        $this->importScenarioDataSet('initialSetup');
+        $this->setUpFrontendRootPage(1);
+        $this->backendUser->workspace = 0;
+    }
+
+    /**
+     * @test
+     */
+    public function assertBehaviourPriorAnyChanges()
+    {
+        $this->actionService->localizeRecord('sys_file_metadata', 1, 1);
+        $this->assertAssertionDataSet('metadataTranslation');
+    }
 
 }
