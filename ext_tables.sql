017c4b0ecc7fd8af2df4f04ffe18a50f76c01e85
Anja Leichsenring <aleichsenring@ab-softlab.de>
2017-03-01 21:06:03 +0100

    [BUGFIX] No NOT NULL to mediumblob field

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE sys_file (
 	# Language fields
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob NOT NULL,
+	l10n_diffsource mediumblob,
 	l10n_state text,
 
 	KEY language (l10n_parent,sys_language_uid)
