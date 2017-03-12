97c77eadf0e8ce312d26c64d8c89d90bdabb14d7
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-03-12 13:16:26 +0100

    Add dummy unit test

---
+++ Tests/Unit/DataHandlerHookTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace T3G\AgencyPack\FileVariants\Tests\Unit;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DataHandlerHookTest
+ * @package T3G\AgencyPack\FileVariants\Tests\Unit
+ */
+class DataHandlerHookTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function dummy()
+    {
+        $this->assertTrue(false);
+    }
+
+}
