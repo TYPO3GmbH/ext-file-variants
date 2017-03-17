9353ef818a8ac15ebfe4be47afd30fe42faea773
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-03-17 18:10:50 +0100

    [TASK] Remove wrong comment in AjaxRoutes.php

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -3,7 +3,6 @@
 use T3G\AgencyPack\FileVariants\Controller\FileVariantsController;
 
 return [
-// Expand or toggle in legacy file tree
     'tx_filevariants_deleteFileVariant' => [
         'path' => '/file_variants/delete_filevariant',
         'target' => FileVariantsController::class . '::ajaxDeleteFileVariant'
