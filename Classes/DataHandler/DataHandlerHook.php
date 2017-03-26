e047ba193ab3bec60013597ed7e024620d745b98
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-03-26 20:09:04 +0200

    [FEATURE] Translating metadata translates file

--- Classes/DataHandler/DataHandlerHook.php
+++ Classes/DataHandler/DataHandlerHook.php
@@ -14,8 +14,11 @@ namespace T3G\AgencyPack\FileVariants\DataHandler;
  *
  * The TYPO3 project - inspiring people to share!
  */
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class DataHandlerHook
 {
@@ -63,9 +66,94 @@ class DataHandlerHook
         DataHandler $pObj
     )
     {
+
+    }
+
+    /**
+     * @param string $command
+     * @param string $table
+     * @param string|int $id recordUid
+     * @param mixed $value Command Value
+     * @param DataHandler $pObj
+     */
+    public function processCmdmap_postProcess(
+        string $command,
+        string $table,
+        $id,
+        $value,
+        DataHandler $pObj
+    )
+    {
         if ($table !== 'sys_file_storage') {
             $this->prepareFileStorageEnvironment();
         }
+
+        // translation of metadata record
+        // results in copied sys_file and relation of record to new file
+        // all references need to be updated to the new file
+        if ($table === 'sys_file_metadata' && $command === 'localize') {
+
+            $id = $this->substNewWithId($id, $pObj);
+            if ($id < 1) {
+                throw new \RuntimeException('can\'t retrieve valid id', 1489332067);
+            }
+
+            /** @var QueryBuilder $queryBuilder */
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_metadata');
+            $queryBuilder->select('uid', 'file')->from('sys_file_metadata')
+                ->where(
+                    $queryBuilder->expr()->eq(
+                        'l10n_parent',
+                        $queryBuilder->createNamedParameter((int)$id, \PDO::PARAM_INT)
+                    ),
+                    $queryBuilder->expr()->eq(
+                        'sys_language_uid',
+                        $queryBuilder->createNamedParameter((int)$value, \PDO::PARAM_INT)
+                    )
+                );
+            $handledMetaDataRecord = $queryBuilder->execute()->fetch();
+
+            $fileUid = (int)$handledMetaDataRecord['file'];
+
+            $parentFile = ResourceFactory::getInstance()->getFileObject($fileUid);
+
+            $translatedFileUid = $parentFile->copyTo($this->folder)->getUid();
+
+            /** @var QueryBuilder $queryBuilder */
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file');
+            $queryBuilder->update('sys_file')->set('sys_language_uid', (int)$value)->set('l10n_parent', $fileUid)->where(
+                $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($translatedFileUid, \PDO::PARAM_INT))
+            )->execute();
+
+            /** @var QueryBuilder $queryBuilder */
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_metadata');
+            $queryBuilder->update('sys_file_metadata')->set('file', $translatedFileUid)->where(
+                $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter((int)$handledMetaDataRecord['uid'], \PDO::PARAM_INT))
+            )->execute();
+
+            /** @var QueryBuilder $queryBuilder */
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
+            $queryBuilder->select('uid')->from('sys_file_reference')->where(
+                $queryBuilder->expr()->eq('sys_language_uid', $queryBuilder->createNamedParameter((int)$value, \PDO::PARAM_INT)),
+                $queryBuilder->expr()->eq('uid_local', $queryBuilder->createNamedParameter($fileUid, \PDO::PARAM_INT))
+            );
+            $references = $queryBuilder->execute()->fetchAll();
+            $filteredReferences = [];
+            foreach ($references as $reference) {
+                $uid = $reference['uid'];
+                if ($this->isValidReference($uid)) {
+                    $filteredReferences[] = $uid;
+                }
+            }
+            foreach ($filteredReferences as $reference) {
+                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBilderForTable('sys_file_reference');
+                $queryBuilder->update('sys_file_reference')->set('file', $fileUid)->where(
+                    $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($reference, \PDO::PARAM_INT))
+                )->execute();
+            }
+        }
+
+
     }
 
     /**
@@ -87,5 +175,43 @@ class DataHandlerHook
         } catch (\InvalidArgumentException $exception) {
             throw new \RuntimeException('storage with uid ' . $storageUid . ' is not available. Create it and check the given uid in extension configuration.', 1490480372);
         }
+    }
+
+    /**
+     * @param string|int $id
+     * @param DataHandler $pObj
+     * @return int
+     */
+    protected function substNewWithId($id, DataHandler $pObj): int
+    {
+        if (is_string($id) && strpos($id, 'NEW') >= 0) {
+            $id = $pObj->substNEWwithIDs[$id];
+        }
+        if ($id === null) {
+            $id = -1;
+        }
+        return $id;
+    }
+
+    /**
+     * Filters away irrelevant tables and checks for free mode in tt_content records
+     * everything else is a valid reference in context of file variants update
+     *
+     * @param int $uid
+     * @return bool
+     */
+    protected function isValidReference(int $uid): bool
+    {
+        $isValid = true;
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
+        $queryBuilder->select('tablenames', 'uid_foreign')->from('sys_file_reference')->where(
+            $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($uid, \PDO::PARAM_INT))
+        );
+        $sysFileReferenceRecord = $queryBuilder->execute()->fetch();
+        $irrelevantTableNames = ['pages', 'pages_language_overlay', 'sys_file_metadata', 'sys_file'];
+        if (in_array($sysFileReferenceRecord['tablenames'], $irrelevantTableNames)) {
+            $isValid = false;
+        }
+        return $isValid;
     }
 }
