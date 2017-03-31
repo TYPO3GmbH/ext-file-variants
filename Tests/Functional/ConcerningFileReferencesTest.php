b09fc8e86cbad0a3ab08ba201a5da0656609cbe1
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-03-31 18:10:08 +0200

    [TASK] Pull out a parent test class for functionals

--- Tests/Functional/ConcerningFileReferencesTest.php
+++ Tests/Functional/ConcerningFileReferencesTest.php
@@ -14,33 +14,16 @@ namespace T3G\AgencyPack\Tests\Functional;
  *
  * The TYPO3 project - inspiring people to share!
  */
-use T3G\AgencyPack\FileVariants\Controller\FileVariantsController;
 use TYPO3\CMS\Core\Core\Bootstrap;
 use TYPO3\CMS\Core\Database\Connection;
-use TYPO3\CMS\Core\Http\Response;
-use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\Security\FileMetadataPermissionsAspect;
 use TYPO3\CMS\Core\Tests\Functional\DataHandling\Framework\ActionService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 class ConcerningFileReferencesTest extends FunctionalTestCase
 {
-    /**
-     * @var int
-     */
-    protected $expectedErrorLogEntries = 0;
 
-    /**
-     * @var ActionService
-     */
-    protected $actionService;
-
-    /**
-     * @var \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
-     */
-    protected $backendUser;
 
     /**
      * @var string
@@ -52,125 +35,6 @@ class ConcerningFileReferencesTest extends FunctionalTestCase
      */
     protected $assertionDataSetDirectory = 'typo3conf/ext/file_variants/Tests/Functional/DataSet/ConcerningFileReferences/AfterOperation/';
 
-    protected function setUp()
-    {
-        $this->testExtensionsToLoad[] = 'typo3conf/ext/file_variants';
-        $this->testExtensionsToLoad[] = 'typo3conf/ext/news';
-
-        parent::setUp();
-
-        // make sure there are no leftover files from earlier tests
-        // done in setup because teardown is called only once per file
-        if (file_exists(PATH_site . 'languageVariants')) {
-            system('rm -rf ' . escapeshellarg(PATH_site . 'languageVariants'));
-        }
-
-        Bootstrap::getInstance()->initializeLanguageObject();
-
-        $this->backendUser = $this->setUpBackendUserFromFixture(1);
-        $fileMetadataPermissionAspect = $this->prophesize(FileMetadataPermissionsAspect::class);
-        GeneralUtility::setSingletonInstance(FileMetadataPermissionsAspect::class,
-            $fileMetadataPermissionAspect->reveal());
-
-        $this->actionService = new ActionService();
-
-        // done to prevent an error during processing
-        // it makes no difference here whether file filters apply to the data set
-        unset($GLOBALS['TCA']['tt_content']['columns']['image']['config']['filter']);
-
-        // set up the second file storage
-        mkdir(PATH_site . 'languageVariants/languageVariants', 0777, true);
-        mkdir(PATH_site . 'languageVariants/_processed_', 0777, true);
-    }
-
-    protected function tearDown()
-    {
-        $this->cleanUpFilesAndRelatedRecords();
-        unset($this->actionService);
-        $this->assertErrorLogEntries();
-        parent::tearDown();
-    }
-
-    /**
-     * remove files and related records (sys_file, sys_file_metadata) from environment
-     */
-    protected function cleanUpFilesAndRelatedRecords()
-    {
-        // find files in storage
-        $storage = ResourceFactory::getInstance()->getStorageObject(2);
-        $recordsToDelete = ['sys_file' => [], 'sys_file_metadata' => []];
-        try {
-            $folder = $storage->getFolder('languageVariants');
-            $files = $storage->getFilesInFolder($folder);
-            foreach ($files as $file) {
-                $storage->deleteFile($file);
-                $recordsToDelete['sys_file'][] = $file->getUid();
-                $metadata = $file->_getMetaData();
-                $recordsToDelete['sys_file_metadata'][] = (int)$metadata['uid'];
-            }
-        } catch (\Exception $exception) {
-            // sometimes, there is no folder to empty. Let's ignore that.
-        }
-        $this->actionService->deleteRecords($recordsToDelete);
-    }
-
-    /**
-     * Asserts correct number of warning and error log entries.
-     *
-     * @return void
-     */
-    protected function assertErrorLogEntries()
-    {
-        if ($this->expectedErrorLogEntries === null) {
-            return;
-        }
-        $queryBuilder = $this->getConnectionPool()
-            ->getQueryBuilderForTable('sys_log');
-        $queryBuilder->getRestrictions()->removeAll();
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('sys_log')
-            ->where(
-                $queryBuilder->expr()->in(
-                    'error',
-                    $queryBuilder->createNamedParameter([1, 2], Connection::PARAM_INT_ARRAY)
-                )
-            )
-            ->execute();
-
-        $actualErrorLogEntries = $statement->rowCount();
-        if ($actualErrorLogEntries === $this->expectedErrorLogEntries) {
-            $this->assertSame($this->expectedErrorLogEntries, $actualErrorLogEntries);
-        } else {
-            $failureMessage = 'Expected ' . $this->expectedErrorLogEntries . ' entries in sys_log, but got ' . $actualErrorLogEntries . LF;
-            while ($entry = $statement->fetch()) {
-                $entryData = unserialize($entry['log_data']);
-                $entryMessage = vsprintf($entry['details'], $entryData);
-                $failureMessage .= '* ' . $entryMessage . LF;
-            }
-            $this->fail($failureMessage);
-        }
-    }
-
-    /**
-     * @param string $scenarioName
-     */
-    protected function importCsvScenario(string $scenarioName = '')
-    {
-        $scenarioFileName = $this->scenarioDataSetDirectory . $scenarioName . '.csv';
-        $scenarioFileName = GeneralUtility::getFileAbsFileName($scenarioFileName);
-        $this->importCSVDataSet($scenarioFileName);
-    }
-
-    /**
-     * @param string $scenarioName
-     */
-    protected function importAssertCSVScenario(string $scenarioName = '')
-    {
-        $scenarioFileName = $this->assertionDataSetDirectory . $scenarioName . '.csv';
-        $scenarioFileName = GeneralUtility::getFileAbsFileName($scenarioFileName);
-        $this->assertCSVDataSet($scenarioFileName);
-    }
 
     /**
      * @test
--- Tests/Functional/ConcerningMetadataTest.php
+++ Tests/Functional/ConcerningMetadataTest.php
@@ -23,25 +23,9 @@ use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\Security\FileMetadataPermissionsAspect;
 use TYPO3\CMS\Core\Tests\Functional\DataHandling\Framework\ActionService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 class ConcerningMetadataTest extends FunctionalTestCase
 {
-    /**
-     * @var int
-     */
-    protected $expectedErrorLogEntries = 0;
-
-    /**
-     * @var ActionService
-     */
-    protected $actionService;
-
-    /**
-     * @var \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
-     */
-    protected $backendUser;
-
     /**
      * @var string
      */
@@ -51,124 +35,6 @@ class ConcerningMetadataTest extends FunctionalTestCase
      * @var string
      */
     protected $assertionDataSetDirectory = 'typo3conf/ext/file_variants/Tests/Functional/DataSet/ConcerningMetadata/AfterOperation/';
-
-    protected function setUp()
-    {
-        $this->testExtensionsToLoad[] = 'typo3conf/ext/file_variants';
-        $this->testExtensionsToLoad[] = 'typo3conf/ext/news';
-
-        parent::setUp();
-
-        // make sure there are no leftover files from earlier tests
-        // done in setup because teardown is called only once per file
-        if (file_exists(PATH_site . 'languageVariants')) {
-            system('rm -rf ' . escapeshellarg(PATH_site . 'languageVariants'));
-        }
-
-        Bootstrap::getInstance()->initializeLanguageObject();
-
-        $this->backendUser = $this->setUpBackendUserFromFixture(1);
-        $fileMetadataPermissionAspect = $this->prophesize(FileMetadataPermissionsAspect::class);
-        GeneralUtility::setSingletonInstance(FileMetadataPermissionsAspect::class, $fileMetadataPermissionAspect->reveal());
-
-        $this->actionService = new ActionService();
-
-        // done to prevent an error during processing
-        // it makes no difference here whether file filters apply to the data set
-        unset($GLOBALS['TCA']['tt_content']['columns']['image']['config']['filter']);
-
-        // set up the second file storage
-        mkdir(PATH_site . 'languageVariants/languageVariants', 0777, true);
-        mkdir(PATH_site . 'languageVariants/_processed_', 0777, true);
-    }
-
-    protected function tearDown()
-    {
-        $this->cleanUpFilesAndRelatedRecords();
-        unset($this->actionService);
-        $this->assertErrorLogEntries();
-        parent::tearDown();
-    }
-
-    /**
-     * remove files and related records (sys_file, sys_file_metadata) from environment
-     */
-    protected function cleanUpFilesAndRelatedRecords() {
-        // find files in storage
-        $storage = ResourceFactory::getInstance()->getStorageObject(2);
-        $recordsToDelete = ['sys_file' => [], 'sys_file_metadata' => []];
-        try {
-            $folder = $storage->getFolder('languageVariants');
-            $files = $storage->getFilesInFolder($folder);
-            foreach ($files as $file) {
-                $storage->deleteFile($file);
-                $recordsToDelete['sys_file'][] = $file->getUid();
-                $metadata = $file->_getMetaData();
-                $recordsToDelete['sys_file_metadata'][] = (int)$metadata['uid'];
-            }
-        } catch (\Exception $exception) {
-            // sometimes, there is no folder to empty. Let's ignore that.
-        }
-        $this->actionService->deleteRecords($recordsToDelete);
-    }
-
-    /**
-     * Asserts correct number of warning and error log entries.
-     *
-     * @return void
-     */
-    protected function assertErrorLogEntries()
-    {
-        if ($this->expectedErrorLogEntries === null) {
-            return;
-        }
-        $queryBuilder = $this->getConnectionPool()
-            ->getQueryBuilderForTable('sys_log');
-        $queryBuilder->getRestrictions()->removeAll();
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('sys_log')
-            ->where(
-                $queryBuilder->expr()->in(
-                    'error',
-                    $queryBuilder->createNamedParameter([1, 2], Connection::PARAM_INT_ARRAY)
-                )
-            )
-            ->execute();
-
-        $actualErrorLogEntries = $statement->rowCount();
-        if ($actualErrorLogEntries === $this->expectedErrorLogEntries) {
-            $this->assertSame($this->expectedErrorLogEntries, $actualErrorLogEntries);
-        } else {
-            $failureMessage = 'Expected ' . $this->expectedErrorLogEntries . ' entries in sys_log, but got ' . $actualErrorLogEntries . LF;
-            while ($entry = $statement->fetch()) {
-                $entryData = unserialize($entry['log_data']);
-                $entryMessage = vsprintf($entry['details'], $entryData);
-                $failureMessage .= '* ' . $entryMessage . LF;
-            }
-            $this->fail($failureMessage);
-        }
-    }
-
-    /**
-     * @param string $scenarioName
-     */
-    protected function importCsvScenario(string $scenarioName = '')
-    {
-        $scenarioFileName = $this->scenarioDataSetDirectory . $scenarioName . '.csv';
-        $scenarioFileName = GeneralUtility::getFileAbsFileName($scenarioFileName);
-        $this->importCSVDataSet($scenarioFileName);
-    }
-
-    /**
-     * @param string $scenarioName
-     */
-    protected function importAssertCSVScenario(string $scenarioName = '')
-    {
-        $scenarioFileName = $this->assertionDataSetDirectory . $scenarioName . '.csv';
-        $scenarioFileName = GeneralUtility::getFileAbsFileName($scenarioFileName);
-        $this->assertCSVDataSet($scenarioFileName);
-    }
 
     /**
      * @test
@@ -233,7 +99,7 @@ class ConcerningMetadataTest extends FunctionalTestCase
 
         copy(PATH_site . 'typo3conf/ext/file_variants/Tests/Functional/Fixture/TestFiles/cat_1.jpg', PATH_site . 'languageVariants/languageVariants/cat_1.jpg');
 
-        mkdir(PATH_site . 'typo3temp/file_variants_uploads/', 0777, true);
+        @mkdir(PATH_site . 'typo3temp/file_variants_uploads/', 0777, true);
         $localFilePath = PATH_site . 'typo3temp/file_variants_uploads/cat_2.jpg';
         copy(PATH_site . 'typo3conf/ext/file_variants/Tests/Functional/Fixture/TestFiles/cat_2.jpg', $localFilePath);
 
@@ -261,7 +127,7 @@ class ConcerningMetadataTest extends FunctionalTestCase
 
         copy(PATH_site . 'typo3conf/ext/file_variants/Tests/Functional/Fixture/TestFiles/cat_1.jpg', PATH_site . 'languageVariants/languageVariants/cat_1.jpg');
 
-        mkdir(PATH_site . 'typo3temp/file_variants_uploads/', 0777, true);
+        @mkdir(PATH_site . 'typo3temp/file_variants_uploads/', 0777, true);
         $localFilePath = PATH_site . 'typo3temp/file_variants_uploads/cat_3.jpg';
         copy(PATH_site . 'typo3conf/ext/file_variants/Tests/Functional/Fixture/TestFiles/cat_3.jpg', $localFilePath);
 
