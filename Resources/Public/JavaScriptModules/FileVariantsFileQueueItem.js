2d1a58299c2b1bce55fda4095851437ea8238972
Sybille Peters <sypets@gmx.de>
2026-01-08 16:18:12 +0100

    [TASK] Clean up JavaScript files

--- Resources/Public/JavaScriptModules/FileVariantsFileQueueItem.js
+++ Resources/Public/JavaScriptModules/FileVariantsFileQueueItem.js
@@ -8,14 +8,11 @@ class FileVariantsFileQueueItem
   performUpload(fileVariantsDragUploader, file, override, handlingUrl, maxFileSize, fileDenyPattern,
                 filesExtensionsAllowed, target) {
 
-    console.log('performUpload');
     var me = this;
     me.fileVariantsDragUploader = fileVariantsDragUploader;
     me.file = file;
     me.override = override;
     me.filesExtensionsAllowed = filesExtensionsAllowed;
-
-    console.log('performUpload: file name=' + this.file.name);
 
     me.updateMessage = function (message) {
       Notification.error('Error', message, 0);
@@ -26,14 +23,11 @@ class FileVariantsFileQueueItem
     }.bind(me);;
 
     me.uploadError = function (response) {
-      console.log('uploadError file.name=' + me.file.name);
       me.updateMessage(TYPO3.lang['file_upload.uploadFailed'].replace(/\{0\}/g, me.file.name));
       var error = $(response.responseText);
       if (error.is('t3err')) {
-        console.log('uploadError: is error');
-        //me.$progressPercentage.text(error.text());
+        me.$progressPercentage.text(error.text());
       } else {
-        console.log('uploadError: statusText=' + response.statusText);
         me.$progressPercentage.text('(' + response.statusText + ')');
       }
       // Cannot read properties of undefined (reading 'addClass')
@@ -42,35 +36,8 @@ class FileVariantsFileQueueItem
     }.bind(me);
 
     me.uploadSuccess = function (data) {
-      console.log('uploadSuccess');
       if (data.upload) {
         FileVariantsDragUploader.processFileVariantUpload(data.upload[0], handlingUrl);
-        //me.fileVariantsDragUploader.processFileVariantUpload(data.upload[0], handlingUrl);
-
-        /*
-        let file = data.upload[0];
-        let url = handlingUrl;
-
-        // temp. workaround (should call fileVariantsDragUploader.processFileVariantUpload instead)
-        var ajaxurl = url + '&file=' + encodeURIComponent(file.uid);
-        $('#t3js-fileinfo').load(ajaxurl, function () {
-          $('.t3js-filevariants-drag-uploader').fileVariantsDragUploader();
-        });
-        $.ajax({
-          url: TYPO3.settings.ajaxUrls['flashmessages_render'],
-          cache: false,
-          success: function (data) {
-            $.each(data, function (index, flashMessage) {
-              Notification.showMessage(flashMessage.title, flashMessage.message, flashMessage.severity);
-            });
-          }
-        });
-
-        // temp woraround end
-        
-         */
-
-
       }
     }.bind(me);
 
@@ -95,15 +62,12 @@ class FileVariantsFileQueueItem
     } else {
 
       var formData = new FormData();
-      console.log('performUpload: target=' + target);
 
       formData.append('data[upload][1][target]', target);
       formData.append('data[upload][1][data]', '1');
       formData.append('overwriteExistingFiles', this.override);
       formData.append('redirect', '');
       formData.append('upload_1', this.file);
-
-      console.log('send ajax request');
 
       var s = $.extend(true, {}, $.ajaxSettings, {
         url: TYPO3.settings.ajaxUrls['file_process'],
@@ -124,39 +88,6 @@ class FileVariantsFileQueueItem
       this.upload = $.ajax(s);
     }
   }
-
-  /*
-  uploadError(response)
-  {
-    this.updateMessage(TYPO3.lang['file_upload.uploadFailed'].replace(/\{0\}/g, this.file.name));
-    var error = $(response.responseText);
-    if (error.is('t3err')) {
-      this.$progressPercentage.text(error.text());
-    } else {
-      this.$progressPercentage.text('(' + response.statusText + ')');
-    }
-    this.$row.addClass('error');
-    this.fileVariantsDragUploader.$trigger.trigger('uploadError', [me, response]);
-  }
-
-  updateMessage(message)
-  {
-    Notification.error('Error', message, 0);
-  }
-
-  uploadStart()
-  {
-    this.fileVariantsDragUploader.$trigger.trigger('uploadStart', [me]);
-  }
-
-  uploadSuccess(data)
-  {
-    if (data.upload) {
-      this.processFileVariantUpload(data.upload[0], handlingUrl);
-    }
-  }
-  */
-
 
   checkAllowedExtensions ()
   {
